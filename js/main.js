f065d0fabad098cf437509338097807c5bc0f9fb
Drine ihssene <drinetn@outlook.com>
2018-04-01 22:22:29 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,10 +67,10 @@ $(document).ready(function(){
 //=====>  Countdown (Edit this with your own date)  <====
 $("#countdown").countdown("2018/06/28", function(event) {
   var $this = $(this).html(event.strftime(''
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Days </div></div> '
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Hours </div></div>'
+     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %-D </span> Jours </div></div> '
+     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %H </span> Heures </div></div>'
      + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %M </span> Minutes </div></div>'
-     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Seconds </div></div>'));
+     + '<div class="countdown-col-wrapper col-xs-3"><div class="countdown-col"><span class="countdown-time"> %S </span> Secondes </div></div>'));
 });
 
 
